e058d69054cc900abdff29c8d5b3b076b722a522
nongsang <shdtkd37@gmail.com>
2017-07-20 14:39:58 +0900

    7월 20일 리암 애니메이션 수정

---
+++ master/Assets/Scripts/START.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class START : MonoBehaviour {
+
+    MeshRenderer mesh;
+    bool enable = false;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enable)
+            mesh.enabled = false;
+    }
+
+    void OnMouseDown()
+    {
+        enable = true;
+    }
+}
