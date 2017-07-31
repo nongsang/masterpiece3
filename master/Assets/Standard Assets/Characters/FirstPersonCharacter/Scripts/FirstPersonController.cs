7f27e6766bb7bf42d23ff20b968e3bc8a5406ea6
nongsang <shdtkd37@gmail.com>
2017-07-31 18:19:31 +0900

    7월 31일 UI

--- master/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ master/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -119,8 +119,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
                                    m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
                 desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
 
-
-
                 m_MoveDir.x = desiredMove.x * speed;
                 m_MoveDir.z = desiredMove.z * speed;
 
