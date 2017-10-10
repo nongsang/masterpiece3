9741772fdeb7351873ce025762481831dddcf4f3
nongsang <shdtkd37@gmail.com>
2017-10-11 03:58:46 +0900

    10월 11일 도로

---
+++ master/Assets/Scripts/ExplosionTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTrigger : MonoBehaviour {
+
+	public GameObject Exp;
+	public GameObject Smoke;
+	public GameObject Fire;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Exp.SetActive(true);
+			Destroy(gameObject, 2.0f);
+			Destroy(Exp, 5.0f);
+			Smoke.SetActive(false);
+			Fire.SetActive(true);
+			InvokeRepeating("FireParticleIncrease", 1.0f, 0.1f);
+		}
+	}
+
+	private void FireParticleIncrease()
+	{
+		Fire.GetComponent<ParticleSystem>().maxParticles += 3;
+	}
+}
